627f3531693afc73c1a5d6a2b99746fa8ebcf92c
Arden Xie <rnx@rnx.sh>
2025-03-17 17:26:49 -0400

    feat: create basic interface styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,65 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
-        <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
-        </a>
+      <div className='title'>
+        <h2>What Should I Do</h2>
+        <h1>Right Now?</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='list'>
+        <div className='list-item'>Test list item 1</div>
+        <div className='list-item'>Test list item 2</div>
+        <div className='list-item'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+          est laborum.
+        </div>
+        <div className='list-item'>
+          Phasellus cursus interdum libero vel rutrum. Vestibulum volutpat, elit quis tristique commodo, ligula ligula
+          dictum odio, varius ultricies felis ligula quis metus. Donec urna quam, aliquam eu risus nec, interdum
+          fermentum mauris. Nullam congue dui sed nunc sodales malesuada ut eget justo. Ut elit tortor, sollicitudin et
+          imperdiet vitae, vehicula nec sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla facilisi.
+          Duis vehicula vehicula placerat. Suspendisse hendrerit metus et elit sodales, id egestas turpis feugiat.
+        </div>
+        <div className='list-item'>
+          Maecenas est felis, consequat in arcu vel, commodo euismod leo. Orci varius natoque penatibus et magnis dis
+          parturient montes, nascetur ridiculus mus. Curabitur eget erat urna. Nulla facilisi. Aenean venenatis sit amet
+          diam non sollicitudin. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos
+          himenaeos. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at lacinia purus, sed eleifend dui.
+          Donec volutpat, mi eu auctor vehicula, lectus orci efficitur neque, vitae feugiat diam elit id neque. Aliquam
+          non tortor orci. Cras ac bibendum orci.
+        </div>
+        <div className='list-item'>
+          Etiam pretium massa sit amet erat aliquet convallis. Phasellus tristique neque ut mauris aliquam, finibus
+          placerat nisl dictum. Quisque eu est purus. Maecenas placerat tellus ultricies convallis commodo. Duis lectus
+          massa, suscipit sed convallis non, blandit eget odio. Pellentesque placerat dolor eros, condimentum faucibus
+          purus congue in. Maecenas fermentum sit amet nisl eu mollis. Proin eget mi quis lectus tincidunt tincidunt sed
+          sed neque. Ut facilisis felis ipsum, at iaculis nisi accumsan non. Cras id neque risus. Proin orci urna,
+          placerat volutpat malesuada vitae, ornare eget eros.
+        </div>
+        <div className='list-item'>
+          Curabitur id posuere diam. Etiam venenatis blandit metus ac fringilla. Maecenas vel luctus lorem. Nunc
+          accumsan arcu diam, sed laoreet ligula pulvinar posuere. Ut congue augue sed purus pharetra cursus. Ut id
+          lobortis arcu. Donec efficitur ex vitae purus porta, in vulputate arcu malesuada. Morbi cursus vestibulum orci
+          eu volutpat. Mauris vulputate nisi at turpis vestibulum dapibus. Vestibulum et neque ut magna feugiat viverra
+          eget sed est. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+          Praesent fringilla dignissim diam, in accumsan orci facilisis eu. Proin bibendum nulla at massa sagittis, in
+          porttitor turpis blandit. Curabitur posuere, nunc quis pretium laoreet, erat lectus egestas lectus, sed
+          venenatis eros neque vitae purus.
+        </div>
+        <div className='list-item'>
+          Aliquam ac nibh non nisi interdum rutrum eget non ligula. Nunc consequat sit amet nibh sed pharetra. Cras
+          ultricies enim vitae libero finibus ullamcorper. Nulla velit ligula, feugiat eget ante a, tincidunt rutrum
+          enim. Ut in turpis est. Aenean ipsum mi, efficitur vel cursus quis, posuere vitae dui. Proin vitae nunc
+          eleifend, rhoncus arcu in, luctus mauris. Nullam ac blandit urna. Aenean facilisis mauris ut velit porta
+          cursus. Aenean malesuada congue neque eu tempus. Nullam faucibus condimentum leo, sed blandit velit posuere
+          eu. Etiam ut arcu diam. Pellentesque dignissim cursus odio eu hendrerit. Curabitur vehicula dui ipsum, vitae
+          pretium quam pellentesque eget. Aliquam erat volutpat.
+        </div>
       </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
     </>
   )
 }
