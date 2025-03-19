cde7c794695e70c59f6819dff9f69ae2ad2a6fa2
Arden Xie <rnx@rnx.sh>
2025-03-18 21:27:21 -0400

    feat: add interactivity to the inputs

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import './App.css'
 
 function App() {
   let [todos, setTodos] = useState(['Hello, world!'])
+  let [todoInput, setTodoInput] = useState('')
+
+  let addTodo = () => {
+    if (todoInput.trim() === '') return
+    setTodos((todos) => [...todos, todoInput])
+    setTodoInput('')
+  }
 
   return (
     <>
@@ -13,8 +20,19 @@ function App() {
             <h1>Right Now?</h1>
           </div>
           <div className='prompt'>
-            <input className='prompt-input' type='text' placeholder='Add a task'></input>
-            <button className='prompt-button'>
+            <input
+              className='prompt-input'
+              type='text'
+              placeholder='Add a task'
+              value={todoInput}
+              onChange={(e) => setTodoInput(e.target.value)}
+              onKeyDown={(e) => {
+                if (e.key === 'Enter') {
+                  addTodo()
+                }
+              }}
+            ></input>
+            <button className='prompt-button' onClick={addTodo}>
               <svg className='prompt-icon' xmlns='http://www.w3.org/2000/svg' fill='#fff' viewBox='0 0 448 512'>
                 <path d='M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z' />
               </svg>
