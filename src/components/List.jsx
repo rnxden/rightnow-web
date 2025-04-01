44ed1a4a7d8b6e5b7cf6824ae324cc4ee8573b96
Arden Xie <rnx@rnx.sh>
2025-04-01 00:09:24 -0400

    fix: prevent shrinking of list item controls on items with large contents

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -10,6 +10,7 @@ function List() {
       <ListItem content='Test List Item 1' />
       <ListItem content='Test List Item 2' />
       <ListItem content='Test List Item 3' />
+      <ListItem content='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.' />
       <ListFooter />
     </div>
   )
